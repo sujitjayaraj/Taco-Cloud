8b26b5524ccb9c64446fc828b86d6733dd27cfa6
Sujit Jayaraj <35429693+sujitjayaraj@users.noreply.github.com>
2024-11-12 21:00:17 +0530

    Getting started with Spring

---
+++ src/test/java/tech/sujitjayaraj/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,31 @@
+package tech.sujitjayaraj.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import tech.sujitjayaraj.tacocloud.controller.HomeController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+class TacoCloudApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("home"))
+				.andExpect(content().string(containsString("Welcome to...")));
+    }
+
+}
