09f603b5bb7c49d9f025b14b6030fd85bc3e1b4e
Sujit Jayaraj <35429693+sujitjayaraj@users.noreply.github.com>
2024-11-12 23:15:23 +0530

    Developing web applications

---
+++ src/main/java/tech/sujitjayaraj/tacocloud/controller/DesignTacoController.java
@@ -0,0 +1,60 @@
+package tech.sujitjayaraj.tacocloud.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tech.sujitjayaraj.tacocloud.domain.Ingredient;
+import tech.sujitjayaraj.tacocloud.domain.Ingredient.Type;
+import tech.sujitjayaraj.tacocloud.domain.Taco;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = List.of(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+        );
+
+        Type[] types = Ingredient.Type.values();
+
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), ingredients.stream()
+                    .filter(x -> x.getType().equals(type))
+                    .toList());
+        }
+
+        model.addAttribute("design", new Taco());
+
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
+}
--- src/test/java/tech/sujitjayaraj/tacocloud/TacoCloudApplicationTests.java
+++ src/test/java/tech/sujitjayaraj/tacocloud/TacoCloudApplicationTests.java
@@ -4,17 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import tech.sujitjayaraj.tacocloud.controller.HomeController;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 class TacoCloudApplicationTests {
 
 	@Autowired
